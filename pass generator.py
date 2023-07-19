ccd9f56c3728b4ed2c5af1f4c29fb51dc34bca43
parthpatyl <parthpatyl@gmail.com>
2023-07-19 16:52:36 +0530

    gui added

--- pass generator.py
+++ pass generator.py
@@ -1,3 +1,4 @@
+import tkinter as tk
 import string
 import random
 
@@ -5,7 +6,28 @@ def generate_password(lenght=8):
     characters = string.ascii_letters+string.digits
     password = ''.join(random.choice(characters) for i in range(lenght))
     return password
-if __name__== "__main__":
-    password_lenght = int(input("Enter the lenght: "))
-    generated_password = generate_password(password_lenght)
-    print(generated_password)
+
+def generate_and_show_password():
+    password_length = int(entry.get())
+    generated_password = generate_password(password_length)
+    password_label.config(text="Generated Password: " + generated_password)
+
+window = tk.Tk()
+window.title("Key Generator")
+window.geometry("350x235")
+window.configure(bg="Grey")
+
+label = tk.Label(window, text="Password Generator", font=("Helvetica",16), fg="purple",padx=10, pady=5)
+label.pack(pady=15)
+
+entry = tk.Entry(window,font=("Arial",14), fg="purple", bg="white",justify="center", width=20)
+entry.pack(padx=5,pady=7)
+
+generate_button = tk.Button(window, text="Generate", command=generate_and_show_password, 
+                            font=("Arial", 12), fg="black", bg="lightgrey", activebackground="pink")
+generate_button.pack(padx=9,pady=15)
+
+password_label = tk.Label(window, text="", font=("Arial",14), fg="purple", bg="white" ,padx=10, pady=5)
+password_label.pack(padx=15,pady=9)
+
+window.mainloop()
